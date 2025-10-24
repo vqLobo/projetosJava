1ea9c30a87212292c63987b30d9c3cc0d00218fd
vqLobo <viniciuslobo.quirino@gmail.com>
2025-10-24 20:47:19 -0300

    atualizando aula24/10

---
+++ aulas/aula2410-coesaoAcoplamento/src/EmployeeManager.java
@@ -0,0 +1,13 @@
+public class EmployeeManager {
+    public void saveToDatabase(Employee e){
+        //logica de salvar no banco de dados
+    }
+
+    public void calculateSalary(Employee e){
+        //logica de calcular salario
+    }
+
+    public void sendEmail(Employee e){
+        //logica de mandar email
+    }
+}
--- aulas/aula2410-coesaoAcoplamento/src/Main.java
+++ aulas/aula2410-coesaoAcoplamento/src/Main.java
@@ -1,5 +1,23 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public class EmployeeRepository {
+        public void saveToDatabase(Employee employee) {
+
+        }
+    }
+
+    public class SalaryCalculator {
+        public void calculateSalary(Employee employee) {
+
+        }
+    }
+
+    public class EmailService {
+        public void sendEmail(Employee employee) {
+
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
